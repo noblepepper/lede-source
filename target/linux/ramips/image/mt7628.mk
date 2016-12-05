08c92006ad2f11d95d3f7e92c049fa8f547a066d
Noble Pepper <gitmail@noblepepper.com>
2016-12-05 04:29:15 -0600

    Setup hardware variants

--- target/linux/ramips/image/mt7628.mk
+++ target/linux/ramips/image/mt7628.mk
@@ -19,20 +19,48 @@ define Device/miwifi-nano
 endef
 TARGET_DEVICES += miwifi-nano
 
+define Device/vocore2-64M
+  DTS := VOCORE2-64M
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  SUPPORTED_DEVICES := vocore2
+  DEVICE_TITLE := Vocore VoCore 2 64M
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
+endef
+TARGET_DEVICES += vocore2-64M
+
+define Device/vocore2-64M-SD
+  DTS := VOCORE2-64M-SD
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  SUPPORTED_DEVICES := vocore2
+  DEVICE_TITLE := Vocore VoCore 2 64M w SD
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
+endef
+TARGET_DEVICES += vocore2-64M-SD
+
+define Device/vocore2-128M
+  DTS := VOCORE2-128M
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  SUPPORTED_DEVICES := vocore2
+  DEVICE_TITLE := Vocore VoCore 2 128M
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
+endef
+TARGET_DEVICES += vocore2-128M
+
+define Device/vocore2-128M-SD
+  DTS := VOCORE2-128M-SD
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  SUPPORTED_DEVICES := vocore2
+  DEVICE_TITLE := Vocore VoCore 2 128M w SD
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
+endef
+TARGET_DEVICES += vocore2-128M-SD
+
 define Device/wl-wn575a3
   DTS := WL-WN575A3
   IMAGE_SIZE := $(ralink_default_fw_size_8M)
   DEVICE_TITLE := Wavlink WL-WN575A3
 endef
 TARGET_DEVICES += wl-wn575a3
-
-define Device/vocore2
-  DTS := VOCORE2
-  IMAGE_SIZE := $(ralink_default_fw_size_16M)
-  DEVICE_TITLE := Vocore VoCore 2
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
-endef
-TARGET_DEVICES += vocore2
 
 define Device/wrtnode2p
   DTS := WRTNODE2P
